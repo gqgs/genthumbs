0af1e2d6cb31d2bc96e8e87500f050180ee7eb80
Guilherme <gqgs@protonmail.com>
2020-12-16 03:59:13 -0300

    fix: preserve natural order while creating tile

--- thumbs.sh
+++ thumbs.sh
@@ -25,7 +25,8 @@ for file in "$@"; do
     while [ $pos -lt $duration ]
     do
         pos=$((pos+start))
-        ffmpeg -loglevel error -skip_frame nokey -ss "$pos"s -i "$file" -vf scale=320:-1 -vsync 0 -frames:v 1 -y "$tmpdir/${file/.*/_$pos.jpg}"
+        possuffix=`printf "%010d" $pos` # padding needed to keep natural sorting order
+        ffmpeg -loglevel error -skip_frame nokey -ss "$pos"s -i "$file" -vf scale=320:-1 -vsync 0 -frames:v 1 -y "$tmpdir/${file/.*/_$possuffix.jpg}"
     done
     ffmpeg -loglevel error -pattern_type glob -i "$tmpdir/*.jpg" -vf tile=4x4:color=white:padding=5:margin=50,drawtext="fontfile=$font: text='$text':fontsize=30:y=10:x=50" -vsync 0 -frames:v 1 -y "${file/.*/.jpg}"
     rm -r "$tmpdir"
