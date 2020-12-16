c8a869aabcc59c731e3bc2bc8234101f0f0bfb6b
Guilherme <gqgs@protonmail.com>
2020-12-16 03:44:42 -0300

    perf: extract frames individually

--- thumbs.sh
+++ thumbs.sh
@@ -7,7 +7,7 @@ for file in "$@"; do
     duration=$(ffprobe "$file" -show_entries format=duration -v quiet -of csv="p=0")
     duration=${duration/.*}
     date="@$duration"
-    duration=$(date -u --date=$date +"%T")
+    timestamp=$(date -u --date=$date +"%T")
 
     size=$(ls -sh "$file")
     size=${size/\ *}
@@ -15,11 +15,18 @@ for file in "$@"; do
     digest=$(sha1sum "$file")
     digest=${digest/\ *}
 
-    text="$size / $duration / $digest"
+    text="$size / $timestamp / $digest"
     text=${text//":"/"\:"}
 
-    total=$(ffprobe -select_streams v:0 "$file" -show_entries stream=nb_frames -of default=nokey=1:noprint_wrappers=1 -v quiet)
-    pos=$((total/20))
+    tmpdir=$(mktemp -d)
 
-    ffmpeg -loglevel error -i "$file" -vf select="isnan(prev_selected_n) + gte(n-prev_selected_n\,$pos)",scale=320:-1,tile=4x4:color=white:padding=5:margin=50,drawtext="fontfile=$font: text='$text':fontsize=30:y=10:x=50" -vsync 0 -frames:v 1 -y "${file/.*/.jpg}"
+    start=$((duration/20))
+    pos=$start
+    while [ $pos -lt $duration ]
+    do
+        pos=$((pos+start))
+        ffmpeg -loglevel error -skip_frame nokey -ss "$pos"s -i "$file" -vf scale=320:-1 -vsync 0 -frames:v 1 -y "$tmpdir/${file/.*/_$pos.jpg}"
+    done
+    ffmpeg -loglevel error -pattern_type glob -i "$tmpdir/*.jpg" -vf tile=4x4:color=white:padding=5:margin=50,drawtext="fontfile=$font: text='$text':fontsize=30:y=10:x=50" -vsync 0 -frames:v 1 -y "${file/.*/.jpg}"
+    rm -r "$tmpdir"
 done
